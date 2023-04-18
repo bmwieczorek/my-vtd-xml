7c194480d866b9044c8966785cbc532f88ee1282
Bartosz Wieczorek <bmwieczorek@gmail.com>
2023-04-18 09:10:01 +0200

    added parsing sum of elements

--- src/test/java/com/bawi/DeclarativeParserTest.java
+++ src/test/java/com/bawi/DeclarativeParserTest.java
@@ -1,9 +1,6 @@
 package com.bawi;
 
-import com.ximpleware.AutoPilot;
-import com.ximpleware.VTDGen;
-import com.ximpleware.VTDNav;
-import com.ximpleware.XPathParseException;
+import com.ximpleware.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -27,7 +24,7 @@ public class DeclarativeParserTest {
     }
 
     interface Parser {
-        Object parse(String field, String xpath, AutoPilot ap, VTDNav nav);
+        Object parse(String field, String xpath, AutoPilot ap, VTDNav nav) throws VTDException;
     }
 
     static class StringLengthParser implements Parser {
@@ -44,13 +41,47 @@ public class DeclarativeParserTest {
         }
     }
 
+    static class SumValuesParser implements Parser {
+
+        @Override
+        public Object parse(String field, String xpath, AutoPilot ap, VTDNav nav) throws VTDException {
+            int sum = 0;
+            if (xpath.startsWith("@") || xpath.contains("/@")) {
+                int i;
+                while((i = ap.evalXPath()) > 0) {
+                    String attrName = nav.toString(i);
+                    int attrIdx = nav.getAttrVal(attrName);
+                    if (attrIdx != -1) {
+                        String value = nav.toString(attrIdx);
+                        int component = Integer.parseInt(value);
+                        sum = sum + component;
+                    }
+                }
+            } else {
+                while((ap.evalXPath()) != -1) {
+                    long attrPosition = nav.getContentFragment();
+                    if (attrPosition != -1) {
+                        String value = nav.toString(nav.getText());
+                        int component = Integer.parseInt(value);
+                        sum = sum + component;
+                    }
+                }
+            }
+            ap.resetXPath();
+            return sum;
+        }
+    }
+
+
     @Test
     public void shouldParseXml() {
         // given
         List<MappingEntry> mapping = List.of(
                 new MappingEntry("college_first_staff_dep_name", "staff[1]/@dep_name", String.class),
                 new MappingEntry("college_id", "@id", Integer.class),
-                new MappingEntry("college_first_staff_dep_name_length", "staff[1]/@dep_name", StringLengthParser.class)
+                new MappingEntry("college_first_staff_dep_name_length", "staff[1]/@dep_name", StringLengthParser.class),
+                new MappingEntry("staff_basic_salary_sum", "staff/salary/basic", SumValuesParser.class),
+                new MappingEntry("staff_id_attr_sum", "staff/@id", SumValuesParser.class)
         );
 
         String xmlFilePath = "src/test/resources/college.xml";
@@ -66,6 +97,8 @@ public class DeclarativeParserTest {
         Assertions.assertEquals(123, result.get("college_id"));
         Assertions.assertEquals("Admin", result.get("college_first_staff_dep_name"));
         Assertions.assertEquals(5, result.get("college_first_staff_dep_name_length"));
+        Assertions.assertEquals(20000 + 25000 + 35000, result.get("staff_basic_salary_sum"));
+        Assertions.assertEquals(101 + 102 + 103, result.get("staff_id_attr_sum"));
     }
 
     private Map<String, Object> parse(String xmlFilePath, List<MappingEntry> mapping) {
@@ -84,11 +117,13 @@ public class DeclarativeParserTest {
             AutoPilot ap = new AutoPilot(nav);
             try {
                 if (Parser.class.isAssignableFrom(entry.clazz)) {
+                    ap.selectXPath(entry.xpath);
                     try {
                         Parser parser = (Parser) entry.clazz.getDeclaredConstructor((Class<?>[]) null).newInstance();
                         Object v = parser.parse(entry.field, entry.xpath, ap, nav);
                         result.put(entry.field, v);
-                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                             NoSuchMethodException | VTDException e) {
                         e.printStackTrace();
                     }
                 } else {
